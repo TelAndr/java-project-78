a92bd0926b3272934b5c617c18bcdb37214de464
Andrey <atelikh@gmail.com>
2026-03-23 22:10:03 +0300

    create methods of StringSchema

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,16 +1,79 @@
 package hexlet.code;
 
-public class StringSchema {
-    public void required() {
-        //
+import java.util.Scanner;
+public class StringSchema extends BaseSchema<String> {
+    public StringSchema(Class<String> type) {
+        super(type);
     }
-    public void minLength(int valMinLength) {
-        if (curString.length() < valMinLength) {
-            System.out.println("String has a length that is less than the minimum");
+
+    @Override
+    protected String cast(Object value) {
+        if (value instanceof String) {
+            return (String) value;
+        } throw new ClassCastException("Value is not a String");
+    }
+    public StringSchema required() {
+        isRunRequired = true;
+        isRequired = true;
+        return this;
+    }
+    public StringSchema minLength(int valMinLength) {
+        isRunMinLength = true;
+        this.curValMinLength = valMinLength;
+        isGreaterMinLength = true;
+        return this;
+    }
+    public StringSchema contains(String curSubstring) {
+        isRunContains = true;
+        this.innerSubstring = curSubstring;
+        isCurContains = true;
+        return this;
+    }
+    @Override
+    protected boolean validate(String curString) {
+        boolean result = true;
+        if (isRequired && (curString == null || curString.isEmpty())) {
+            result = false;
         }
+        if (isGreaterMinLength &&  (curString.length() < curValMinLength)) {
+            result = false;
+        }
+        if (isCurContains && (innerSubstring != null && !curString.contains(innerSubstring))) {
+            result = false;
+        }
+        return result;
     }
-    public void contains(String curSubstring) {
-        //
+    //public boolean isValid() {
+    //    String input;
+    //    while (true) {
+    //        System.out.print("Введите данные (обязательно): ");
+    //        input = scanner.nextLine().trim(); // Убираем пробелы по краям
+    //        curString = input;
+    //        if (validate(curString)) {
+    //            return true;
+    //        } else {
+    //            System.out.println("Некорректный ввод. Повторите снова.");
+    //        }
+    //    }
+    //}
+    @Override
+    public boolean isValid(Object value) {
+        if (value == null) {
+            return !isRequired; // например, null допустим, если не required
+        }
+        if (!(value instanceof String)) {
+            return false; // тип не совпадает
+        }
+        return validate((String) value);
     }
     private String curString;
+    private int curValMinLength;
+    private String innerSubstring;
+    private boolean isRequired;
+    private boolean isGreaterMinLength;
+    private boolean isCurContains;
+    private boolean isRunRequired;
+    private boolean isRunMinLength;
+    private boolean isRunContains;
+    private static Scanner scanner = new Scanner(System.in);
 }
