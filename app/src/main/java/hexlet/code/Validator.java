39426c7dc5c351f43cf3a4044f3ce598b39ba33c
Andrey <atelikh@gmail.com>
2026-03-23 22:11:16 +0300

    create methods of Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -2,7 +2,8 @@ package hexlet.code;
 
 public class Validator {
     public StringSchema string() {
-        return new StringSchema();
+        return new StringSchema(String.class);
     }
-    public NumberSchema number() { return new NumberSchema(); }
+    public NumberSchema number() { return new NumberSchema(Integer.class); }
+    public MapSchema map() { return new MapSchema(); }
 }
