ed64af61f85e2b7bcb0d487b965b3b87949eb8d6
Andrey <atelikh@gmail.com>
2026-04-05 23:09:44 +0300

    add javadoc to string, number, map

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -1,9 +1,24 @@
 package hexlet.code;
 
 public class Validator {
+    /**
+     * определяет схему StringSchema. Эта схема используется для валидации строк.
+     *
+     * @return возвращает объект StringSchema
+     */
     public StringSchema string() {
         return new StringSchema(String.class);
     }
+    /**
+     * определяет схему NumberSchema. Эта схема используется для валидации чисел.
+     *
+     * @return возвращает объект NumberSchema
+     */
     public NumberSchema number() { return new NumberSchema(Integer.class); }
+    /**
+     * определяет схему MapSchema. Эта схема используется для валидации объектов типа Map.
+     *
+     * @return возвращает объект MapSchema
+     */
     public MapSchema map() { return new MapSchema(); }
 }
