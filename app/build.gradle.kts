4942564392009f60a2579155319b8831bfc2e7ab
Andrey <atelikh@gmail.com>
2026-04-18 22:36:33 +0300

    add dependencies assertj-core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ application {
 dependencies {
     implementation("org.projectlombok:lombok:1.18.26")
     implementation("org.testng:testng:7.1.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
