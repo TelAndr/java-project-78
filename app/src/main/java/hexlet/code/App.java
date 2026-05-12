94d98dc639e7330764688397a9ad446bd2de7f17
Andrey <atelikh@gmail.com>
2026-05-12 22:50:05 +0300

    comment rows import

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
-import hexlet.code.schemas.BaseSchema;
+//import hexlet.code.schemas.BaseSchema;
 //import hexlet.code.schemas.NumberSchema;
-import hexlet.code.schemas.StringSchema;
+//import hexlet.code.schemas.StringSchema;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
