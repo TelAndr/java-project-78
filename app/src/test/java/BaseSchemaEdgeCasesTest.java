4b1f2913dccc80b425bcf721b2bfe6969d5c269b
Andrey <atelikh@gmail.com>
2026-06-10 00:25:19 +0300

    comment nonused imports

--- app/src/test/java/BaseSchemaEdgeCasesTest.java
+++ app/src/test/java/BaseSchemaEdgeCasesTest.java
@@ -3,11 +3,11 @@ import hexlet.code.Validator;
 import hexlet.code.schemas.StringSchema;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
+//import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
+//import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
